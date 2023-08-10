16620789d24f912475fa2e3272d5d6363786c8bf
Paul Byrne <pbyrne@razoyo.com>
2023-08-10 10:32:09 -0500

    added click off to exit modal by clicking x or just the stuff around it

--- lib/grass_farmer_web/components/schedules/schedule_manager.ex
+++ lib/grass_farmer_web/components/schedules/schedule_manager.ex
@@ -29,15 +29,15 @@ defmodule GrassFarmerWeb.Components.ScheduleManager do
   attr :myself, :map, required: true
   def schedule_modal_form(assigns) do
     ~H"""
-      <div class="flex justify-center h-screen w-screen top-0 left-0 fixed items-center bg-green-200/75 antialiased">
-      <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
+      <div class="flex justify-center h-screen w-screen top-0 left-0 fixed items-center bg-green-200/75 antialiased" phx-click="cancel_edit" phx-target={@myself}>
+      <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl" phx-click="" phx-target={@myself}>
         <div
           class="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg"
         >
           <p class="font-semibold text-gray-800">Add a schedule</p>
-          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" phx-click="cancel_edit" phx-target={@myself}>
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
+          </svg>
         </div>
         <div class="flex flex-col px-6 py-5 bg-gray-50">
           <p class="mb-2 font-semibold text-gray-700">Name</p>
@@ -79,5 +79,10 @@ defmodule GrassFarmerWeb.Components.ScheduleManager do
 
 
     {:noreply, assign(socket, %{schedules: schedules, edit: true})}
+  end
+
+  @impl true
+  def handle_event("cancel_edit", _params, socket) do
+    {:noreply, assign(socket, %{edit: false})}
   end
 end
